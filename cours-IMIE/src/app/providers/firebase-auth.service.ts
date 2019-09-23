afb7ca6e549f9ca620d6994ddeff509f4c217e84
Tuturlolilol <arthurbetito@gmail.com>
2019-09-23 15:26:58 +0200

    Initial commit

---
+++ cours-IMIE/src/app/providers/firebase-auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { Observable } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseAuthService {
+
+  constructor(private authFire: AngularFireAuth) { }
+
+  SignUp(email: string, password: string){
+
+    this.authFire.auth.createUserWithEmailAndPassword(email, password)
+    .then( reponse => {
+
+      console.log(reponse)
+
+    } )
+    .catch(error => {
+      console.log("Error:" + error)
+    })
+    
+  }
+}
